0015deb1c4173aedd1929241d0b4771b83ad5b4f
George Anagnostou <anagnostougeorgejohn@gmail.com>
2026-06-14 11:06:00 -0700

    cmd: add http tests

--- cmd/bookmarkd/http_test.go
+++ cmd/bookmarkd/http_test.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
+	"io"
+	"log"
 	"net/http"
 	"testing"
 )
@@ -51,13 +54,103 @@ func TestNewHTTPServer(t *testing.T) {
 }
 
 func TestServeHTTPReturnsListenError(t *testing.T) {
+	wantErr := errors.New("listen failed")
+
+	srv := fakeHTTPServer{
+		listenAndServe: func() error {
+			return wantErr
+		},
+		shutdown: func(ctx context.Context) error {
+			t.Fatal("Shutdown should not be called")
+			return nil
+		},
+	}
+
+	err := serveHTTP(context.Background(), srv, "127.0.0.1:8080", testLogger())
+	if !errors.Is(err, wantErr) {
+		t.Fatalf("err = %v, want %v", err, wantErr)
+	}
 }
 
 func TestServeHTTPIgnoresServerClosed(t *testing.T) {
+	srv := fakeHTTPServer{
+		listenAndServe: func() error {
+			return http.ErrServerClosed
+		},
+		shutdown: func(ctx context.Context) error {
+			t.Fatal("Shutdown should not be called")
+			return nil
+		},
+	}
+
+	if err := serveHTTP(context.Background(), srv, "127.0.0.1:8080", testLogger()); err != nil {
+		t.Fatalf("err = %v, want nil", err)
+	}
 }
 
-func TestServeHTTPShutsDownWHenContextCanceled(t *testing.T) {
+func TestServeHTTPShutsDownWhenContextCanceled(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	listening := make(chan struct{})
+	shutdownCalled := make(chan struct{})
+
+	srv := fakeHTTPServer{
+		listenAndServe: func() error {
+			close(listening)
+			<-shutdownCalled
+			return http.ErrServerClosed
+		},
+		shutdown: func(ctx context.Context) error {
+			close(shutdownCalled)
+			return nil
+		},
+	}
+
+	errc := make(chan error, 1)
+	go func() {
+		errc <- serveHTTP(ctx, srv, "127.0.0.1:8080", testLogger())
+	}()
+
+	<-listening
+	cancel()
+
+	if err := <-errc; err != nil {
+		t.Fatalf("err = %v, want nil", err)
+	}
 }
 
 func TestServeHTTPReturnsShutdownError(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	wantErr := errors.New("shutdown failed")
+
+	listening := make(chan struct{})
+	releaseServer := make(chan struct{})
+
+	srv := fakeHTTPServer{
+		listenAndServe: func() error {
+			close(listening)
+			<-releaseServer
+			return http.ErrServerClosed
+		},
+		shutdown: func(ctx context.Context) error {
+			close(releaseServer)
+			return wantErr
+		},
+	}
+
+	errc := make(chan error, 1)
+	go func() {
+		errc <- serveHTTP(ctx, srv, "127.0.0.1:8080", testLogger())
+	}()
+
+	<-listening
+	cancel()
+
+	if err := <-errc; !errors.Is(err, wantErr) {
+		t.Fatalf("err = %v, want %v", err, wantErr)
+	}
+}
+
+func testLogger() *log.Logger {
+	return log.New(io.Discard, "", 0)
 }
